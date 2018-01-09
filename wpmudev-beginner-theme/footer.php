631fe1dece56b6ba36a0767a41018ff74c240d85
Kamil Pacanek <KamilPacanek@users.noreply.github.com>
2018-01-09 21:04:01 +0100

    Basic theme structure, basic styling

---
+++ wpmudev-beginner-theme/footer.php
@@ -0,0 +1,11 @@
+</div> <!-- site content -->
+
+<div id='site-footer'>
+<p>&copy; Custom Theme by Kamil Pacanek for
+  <a href='https://premium.wpmudev.org/academy/courses/wordpress-development-for-beginners-3/units/'>wpmudev tutorial</a>
+</p>
+</div>
+
+<?php wp_footer() ?>
+</body>
+</html>
---
+++ wpmudev-beginner-theme/index.php
@@ -0,0 +1,18 @@
+<?php get_header() ?>
+
+<p>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt a purus a vulputate. Curabitur eu pulvinar justo, in cursus ante. In accumsan augue urna, sed molestie tellus pharetra dictum. Quisque non dolor nisl. Morbi tempor ipsum sed risus tincidunt pharetra ac a justo. Duis nec ligula quis sem dignissim eleifend. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas ullamcorper magna vestibulum urna efficitur rhoncus. Pellentesque sollicitudin fringilla sapien eget tincidunt. Etiam accumsan ipsum velit, et laoreet nisi mattis eget. Donec lacinia est dictum nulla vestibulum, id ultrices turpis rutrum. Duis laoreet aliquet mattis. Etiam cursus lacus a lacus viverra viverra. Nunc faucibus vitae risus eget elementum. Phasellus sollicitudin ac lorem sit amet ultrices.
+</p>
+<p>
+Vivamus non faucibus enim, eget fermentum mauris. Nullam ac orci malesuada, gravida massa ac, vestibulum arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer eros justo, pretium eget lacus a, tempus euismod libero. Aenean vel vulputate lorem. Nullam velit libero, elementum sed euismod eget, ornare ac orci. Nulla egestas cursus nibh sit amet posuere. In sagittis dapibus metus at eleifend. Duis molestie felis sapien, vitae vestibulum est rutrum sed. Sed et ornare est. Mauris sagittis sed tellus eget pulvinar.
+</p>
+<p>
+In aliquam scelerisque rhoncus. Donec lobortis, neque ut posuere accumsan, dolor est rutrum odio, ut interdum lacus ipsum at eros. Quisque facilisis nulla lacus, vel blandit massa tincidunt non. Duis scelerisque ut felis vulputate eleifend. Sed vitae molestie sapien. Duis ac malesuada velit. Nam malesuada diam sed sollicitudin porttitor.
+</p>
+<p>
+Integer tortor eros, consequat in mollis vel, fermentum et mi. Nullam fermentum quis justo non fermentum. Donec tristique augue at velit egestas iaculis. Pellentesque eu mi maximus, varius arcu vel, condimentum urna. Nam laoreet, nibh vel dignissim porta, lacus felis volutpat augue, vel vulputate enim ipsum vitae neque. Donec a interdum libero, non viverra tortor. Nunc ullamcorper ac risus sit amet vestibulum. Sed laoreet, felis sed porta finibus, nisi nibh efficitur massa, sit amet condimentum magna nunc eu libero. Suspendisse in ornare odio. Nulla at justo eget purus vehicula rhoncus. Nulla aliquam nulla quis accumsan venenatis.
+</p>
+<p>
+Curabitur ac sagittis velit, porttitor mattis justo. Fusce eu fringilla lorem, vitae porttitor massa. Suspendisse viverra tortor id purus malesuada, sed fringilla mauris bibendum. Duis a finibus tortor, a tincidunt tortor. Sed et efficitur urna. Morbi feugiat dui et lorem egestas, ac consectetur nibh malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+</p>
+<?php get_footer() ?>
