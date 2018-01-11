6f03dbdd3c28a04160659829b2ee4438bdbc3b4d
Kamil Pacanek <KamilPacanek@users.noreply.github.com>
2018-01-11 21:12:25 +0100

    External link in footer opens in new tab

--- wpmudev-beginner-theme/footer.php
+++ wpmudev-beginner-theme/footer.php
@@ -1,12 +1,12 @@
   </div> <!-- site content -->
 
   <?php get_sidebar(); ?>
-  
+
 </div> <!-- site container -->
 
 <div id='site-footer'>
 <p>&copy; Custom Theme by Kamil Pacanek for
-  <a href='https://premium.wpmudev.org/academy/courses/wordpress-development-for-beginners-3/units/'>wpmudev tutorial</a>
+  <a target="_blank" href='https://premium.wpmudev.org/academy/courses/wordpress-development-for-beginners-3/units/'>wpmudev tutorial</a>
 </p>
 </div>
 
